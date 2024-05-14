f808a3b69a390e79cc5cf84964415ed0cc7f970b
Johanna <johanna.engman@math.lth.se>
2024-05-14 11:30:42 +0200

    updaterade sbatch script

--- train_resnet.sh
+++ train_resnet.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #SBATCH --gpus 1
 #SBATCH -t 2-00:00:00
+
+# job name
+#SBATCH -J Train_resnet_1
+#
+# Remap stdout and stderr to write to these files
+#SBATCH -o Train_resnet_%A_%a.out
+#SBATCH -e Train_resnet_%A_%a.out
  
 module load Anaconda/2023.09-0-hpc1-bdist 
 conda activate dcase
