91d735dff5cc40cf445cbce4496486fb3c949042
Johanna <johanna.engman@math.lth.se>
2024-05-22 08:53:15 +0200

    updated the model

--- cls_vid_features.py
+++ cls_vid_features.py
@@ -11,11 +11,16 @@ class VideoFeatures(nn.Module):
         self.model = torch.nn.Sequential(*(list(self.model.children())[:-2]))
 
         # Concatenate the ResNet model with the convolutional layer
-        conv_layer = nn.Conv2d(2048, 156, kernel_size=1)
-        self.model = torch.nn.Sequential(self.model, conv_layer)
+        conv_layer_1 = nn.Conv2d(2048, 1024, kernel_size=1)
+        conv_layer_2 = nn.Conv2d(1024, 128, kernel_size=1)
+        conv_layer_3 = nn.Conv2d(128, 1, kernel_size=1)
+        conv_layer_up = nn.Conv2d(1, 156, kernel_size=1)
+
+        # Concatenate the ResNet model with the new convolutional layers
+        self.model = torch.nn.Sequential(self.model, conv_layer_1, conv_layer_2, conv_layer_3, conv_layer_up)
 
         # Load pre-trained weights
-        weights_path = 'model_100epochs.pth'
+        weights_path = 'final_model_down_to_1.pth'
         self.model.load_state_dict(torch.load(weights_path))
 
         # Remove the last added conv_layer
@@ -34,11 +39,12 @@ class VideoFeatures(nn.Module):
             iter = (len(preprocessed_images) - 1) // max_batch_size + 1
             vid_features_part_list = []
             for i in range(iter):
-                preprocessed_images_part = torch.stack(preprocessed_images[i * max_batch_size: (i + 1) * max_batch_size], dim = 0)
+                preprocessed_images_part = torch.stack(preprocessed_images[i * max_batch_size: (i + 1) * max_batch_size], dim=0)
                 vid_features_part = self.model(preprocessed_images_part)
                 #vid_features_part = torch.mean(vid_features_part, dim=1)
+                vid_features_part = torch.squeeze(vid_features_part, dim=1)
                 vid_features_part_list.append(vid_features_part)
-            vid_features = torch.cat(vid_features_part_list, dim = 0)
+            vid_features = torch.cat(vid_features_part_list, dim=0)
             return vid_features
 
 
