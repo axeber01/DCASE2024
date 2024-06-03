135af73e82c67e73447ef81e27c2c3e6981a6993
Johanna <johanna.engman@math.lth.se>
2024-06-03 14:13:12 +0200

    using all classes in visual loss

--- seldnet_model.py
+++ seldnet_model.py
@@ -18,7 +18,7 @@ class MSELoss_ADPIT(object):
         self.eps = 0.001
 
         # relevant classes are [0 -female speech, 1-male speech, 2-clapping, 4-laugh, 6-footsteps, 9-music instrument]
-        self.visual_classes = [0, 1, 2, 4, 6, 9]
+        self.visual_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     def _each_calc(self, output, target):
         loss = self._each_loss(output, target)
